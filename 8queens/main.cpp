ca0e04f917a85e4cb007bab490c23b6664ddcd38
amir hosein moradi <rahaansharif01@gmail.com>
2025-06-11 21:34:15 +0330

    now, the tree structure is completed. it can use back-tracking method to make nodes.

--- 8queens/main.cpp
+++ 8queens/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 using namespace std;
+#define dimentions 8
 
 class tree
 {
@@ -16,40 +17,64 @@ public:
         x=x_in;
         y=y_in;
         parent=NULL;
-        children=new tree*[8];
+        children=new tree*[dimentions];
     }
     tree(tree*& parent_in, int x_in, int y_in)
     {
         x=x_in;
         y=y_in;
         parent=parent_in;
-        children=new tree*[8];
+        children=new tree*[dimentions];
     }
-    void make_root(tree* root)
+    void make_root(tree*& root)
     {
+        cout<<"make root function: \n";
         root=new tree(-1, -1);
-        for(int i=0; i<8; i++)
+        for(int i=0; i<dimentions; i++)
         {
-            root->children[i]->make_node(root, root->children[i], i, 1);
+            root->children[i]->make_node(root, root->children[i], i, 0);
+            cout<<"------------\n";
         }
     }
     void make_node(tree*& parent, tree*& node, int x_in, int y_in)
     {
+        if(y_in>=dimentions  || parent->x==x_in ||
+           parent->x==x_in-1 || parent->x==x_in+1)
+            return;
+
         node=new tree(parent, x_in, y_in);
         node->parent=parent;
-        if(y_in>=8)
-            return;
-        for(int i=0; i<8; i++)
 
+        cout<<"make node function:  ("<<node->x<<","<<node->y<<")\n";
+
+        for(int i=0; i<dimentions; i++)
+        {
+            node->children[i]->make_node(node, node->children[i], i, node->y+1);
+        }
     }
-    void show()
-    {}
+    void show(tree*& root)
+    {
+        if(root==NULL)
+        {
+            return;
+        }
+        cout<<"("<<root->x<<","<<root->y<<") -> ";
 
+        for(int i=0; i<dimentions; i++)
+        {
+            root->children[i]->show(root->children[i]);
+        }
+    }
 };
 
 
 int main(void)
 {
+    cout<<"main function\n";
+    tree* root=NULL;
+    root->make_root(root);
+    cout<<endl<<endl;
+    root->show(root);
 
 
     return 0;
