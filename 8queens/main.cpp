73d6986f0671766b4f923e3ae00b4c34a7410d36
amir hosein moradi <rahaansharif01@gmail.com>
2025-06-11 23:04:18 +0330

    program is finished. It is working properly

--- 8queens/main.cpp
+++ 8queens/main.cpp
@@ -2,6 +2,54 @@
 using namespace std;
 #define dimentions 8
 
+class linked_list
+{
+private:
+    int x, y;
+    linked_list* next;
+public:
+    linked_list(int x_in, int y_in)
+    {
+        x=x_in;
+        y=y_in;
+        next=NULL;
+    }
+    void make_node(linked_list*& start, int x_in, int y_in)
+    {
+        if(start==NULL)
+        {
+            start=new linked_list(x_in, y_in);
+        }
+        else
+        {
+            linked_list* tmp_ll=start;
+            while(tmp_ll->next!=NULL)
+            {
+                tmp_ll=tmp_ll->next;
+            }
+            tmp_ll->next=new linked_list(x_in, y_in);
+        }
+    }
+    void show(linked_list* start)
+    {
+        cout<<"(x,y):"<<endl;
+        start=start->next;
+        if(start==NULL)
+        {
+            cout<<"null list\n";
+            return;
+        }
+        while(start->next!=NULL)
+        {
+            cout<<"("<<start->x<<","<<start->y<<") -> ";
+            start=start->next;
+        }
+        cout<<"("<<start->x<<","<<start->y<<")\n";
+    }
+};
+
+linked_list* start_ll=NULL;
+
 class tree
 {
 private:
@@ -28,53 +76,62 @@ public:
     }
     void make_root(tree*& root)
     {
-        cout<<"make root function: \n";
         root=new tree(-1, -1);
         for(int i=0; i<dimentions; i++)
         {
-            root->children[i]->make_node(root, root->children[i], i, 0);
-            cout<<"------------\n";
+            if(root->children[i]->make_node(root, root->children[i], i, 0))
+            {
+                start_ll->make_node(start_ll, i, 0);
+                return;
+            }
         }
     }
-    void make_node(tree*& parent, tree*& node, int x_in, int y_in)
+    int make_node(tree*& parent, tree*& node, int x_in, int y_in)
     {
-        if(y_in>=dimentions  || parent->x==x_in ||
-           parent->x==x_in-1 || parent->x==x_in+1)
-            return;
+        if(y_in>=dimentions)
+        {
+            return 1;
+        }
+
+        tree* tmp_tree=parent;
+        while(tmp_tree!=NULL && tmp_tree->x!=-1 && tmp_tree->y!=-1)
+        {
+            if(tmp_tree->x - x_in  == tmp_tree->y - y_in)
+            {
+                return 0;
+            }
+            else if(tmp_tree->x - x_in  == -(tmp_tree->y - y_in))
+            {
+                return 0;
+            }
+            else if(tmp_tree->x==x_in  ||  tmp_tree->y==y_in)
+            {
+                return 0;
+            }
+            tmp_tree=tmp_tree->parent;
+        }
 
         node=new tree(parent, x_in, y_in);
         node->parent=parent;
 
-        cout<<"make node function:  ("<<node->x<<","<<node->y<<")\n";
-
         for(int i=0; i<dimentions; i++)
         {
-            node->children[i]->make_node(node, node->children[i], i, node->y+1);
+            if(node->children[i]->make_node(node, node->children[i], i, node->y+1))
+            {
+                start_ll->make_node(start_ll, i, node->y+1);
+                return 1;
+            }
         }
-    }
-    void show(tree*& root)
-    {
-        if(root==NULL)
-        {
-            return;
-        }
-        cout<<"("<<root->x<<","<<root->y<<") -> ";
-
-        for(int i=0; i<dimentions; i++)
-        {
-            root->children[i]->show(root->children[i]);
-        }
+        return 0;
     }
 };
 
 
 int main(void)
 {
-    cout<<"main function\n";
     tree* root=NULL;
     root->make_root(root);
-    cout<<endl<<endl;
-    root->show(root);
+    start_ll->show(start_ll);
 
 
     return 0;
